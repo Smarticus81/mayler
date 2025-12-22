ae8f35b70992567504a28797b957e61590a2fb4e
Smarticus81 <tmuso@smarticus.com>
2025-12-22 03:23:52 -0600

    Increase express body size limit to 50mb

--- server.js
+++ server.js
@@ -35,9 +35,8 @@ console.log('Services loaded');
 
 // Enable CORS for all routes
 app.use(cors());
-app.use(express.json());
-
-// Initialize Routers
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 const tokenRouter = createTokenRouter();
 const gmailRouter = createGmailRouter(gmailService);
 const calendarRouter = createCalendarRouter(gmailService);
