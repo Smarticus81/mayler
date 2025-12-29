72430d97962f6a674fe263a1856bc433cafb31e7
Smarticus81 <tmuso@smarticus.com>
2025-12-29 08:54:54 -0600

    fix: Add missing express-session import and dependency

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
